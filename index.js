a8658d9413fab2c52819bbbadf4d7ee014ab2e0e
Ranjeet Rocky <ranjeetrocky2000@gmail.com>
2021-12-15 15:53:08 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const port = process.env.port || 4200
+
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send("home")
+})
+app.get('/about',(req,res)=>{
+    res.send("about")
+})
+app.get('*',(req,res)=>{
+    res.send("Any Other PAGE")
+})
+app.listen(port,()=>{
+    console.log("Listning at port "+port)
+})
