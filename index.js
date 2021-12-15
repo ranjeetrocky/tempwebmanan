efe017e00bcac86aa868c07ad6f52e3ef17918f6
Ranjeet Rocky <ranjeetrocky2000@gmail.com>
2021-12-15 16:17:13 +0530

    added heroku support

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const port = process.env.port || 4200
 
 const app = express()
 
-app.get('/',(req,res)=>{
-    res.send("home")
-})
-app.get('/about',(req,res)=>{
-    res.send("about")
-})
+app.use(express.static('public'))
+
+// app.get('/',(req,res)=>{
+//     res.send("home")
+// })
+// app.get('/about',(req,res)=>{
+//     res.send("about")
+// })
 app.get('*',(req,res)=>{
     res.send("Any Other PAGE")
 })
